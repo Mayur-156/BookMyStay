01e1df0d6980a0069dba3ca652d4cf13b9b0920f
Mayur-156 <mayurkadammsk380@gmail.com>
2025-12-28 00:52:18 +0530

    ADD Navbar, logo, create folder includes, public for css and images

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodoverride =require("method-override");
+const ejsMate = require("ejs-mate");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/bookMyStay"
@@ -25,6 +26,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodoverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.get("/", (req, res) => {
     res.send("hi , I am Root");
